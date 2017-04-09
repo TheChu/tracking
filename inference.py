dab5d5345b4b0b01b2bfe274f818e99f5f34a029
David Chu <chu.david.a@gmail.com>
2017-04-08 17:53:27 -0700

    Implemented DiscreteDistribution Class

--- inference.py
+++ inference.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -75,6 +75,10 @@ class DiscreteDistribution(dict):
         {}
         """
         "*** YOUR CODE HERE ***"
+        total = self.total()
+        if total != 0:
+            for key, value in self.iteritems():
+                self[key] = value / total
 
     def sample(self):
         """
@@ -98,7 +102,14 @@ class DiscreteDistribution(dict):
         0.0
         """
         "*** YOUR CODE HERE ***"
-
+        r = random.random()
+        distCopy = self.copy()
+        distCopy.normalize()
+        currentSum = 0
+        for key, value in distCopy.iteritems():
+            currentSum += value
+            if r <= currentSum:
+                return key
 
 class InferenceModule:
     """
